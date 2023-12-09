bf089e03fc5f53b19700ab82c230ecf2e30e6ba5
hekate <phyzheye01@gmail.com>
2023-12-09 09:01:40 -0700

    added correct file

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import './style.css'
 
-const URL = '/Boleta parcial.pdf'
+const URL = '/Boleta-periodo-Agosto-Diciembre-2023-3.pdf'
 
 function downloadFile(e) {
   e.preventDefault();
   const a = document.createElement('a')
   a.href = URL
-  a.download = 'Boleta.pdf'
+  a.download = 'Boleta-periodo-Agosto-Diciembre-2023-3.pdf'
   document.body.appendChild(a)
   a.click()
   document.body.removeChild(a)
